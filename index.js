c37afa6f719026149174ec764e048f60afae64bc
Raynos <raynos2@gmail.com>
2012-06-04 13:29:43 +0100

    refactor again

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-// Third party
 var ErrorPage = require("error-page"),
-    Negotiator = require("negotiator"),
     Templar = require("templar"),
     extend = require("xtend"),
-    // node
-    querystring = require("querystring"),
-    // internal
-    static = require("./lib/static"),
+    Static = require("./lib/static"),
+    Body = require("./lib/body"),
+    MediaTypes = require("./lib/mediaTypes"),
+    Redirect = require("./lib/redirect"),
+    Methods = require("./lib/methods"),
     encoding = require("./lib/encoding"),
     contentTypes = require("./lib/contentTypes"),
     body = require("./lib/body"),
     send = require("./lib/send"),
     sendJson = send.sendJson,
     sendHtml = send.sendHtml,
-    // variables
     isJSON = /\/(x-)?json$/,
     isForm = /application\/x\-www\-form\-urlencoded/
     
@@ -21,9 +19,24 @@ module.exports = Routil()
 
 function Routil(options) {
     var config = extend({
-        errorPage: {},
-        templar: {}
-    }, options || {})
+            errorPage: {},
+            templar: {}
+        }, options || {}),
+        body = Body({
+            errorPage: errorPage
+        }),
+        static = Static({ 
+            errorPage: errorPage 
+        }),
+        methods = Methods({
+            errorPage: errorPage
+        }),
+        mediaTypes = MediaTypes({
+            errorPage: errorPage
+        }),
+        redirect = Redirect({
+            mediaTypes: mediaTypes
+        })
 
     return {
         config: configure,
@@ -33,7 +46,6 @@ function Routil(options) {
         redirect: redirect,
         mediaTypes: mediaTypes,
         static: static,
-        Static: static.Static,
         contentTypes: contentTypes,
         encoding: encoding,
         sendJson: sendJson,
@@ -42,9 +54,9 @@ function Routil(options) {
         methods: methods,
         template: template,
         Templar: Templar,
-        body: body,
-        formBody: formBody,
-        jsonBody: jsonBody
+        body: body.body,
+        formBody: body.formBody,
+        jsonBody: body.jsonBody
     }
 
     function errorPage(req, res, details) {
@@ -56,129 +68,11 @@ function Routil(options) {
         }
     }
 
-    function redirect(req, res, target, statusCode) {
-        statusCode = statusCode || 302
-        res.statusCode = statusCode
-        res.setHeader('location', target)
-
-        mediaTypes(req, res, {
-            "application/json": jsonRedirectHandler,
-            "default": defaultRedirectHandler
-        })(res, target, statusCode)
-    }
-
-    function jsonRedirectHandler() {
-        sendJson(res, {
-            redirect: target,
-            statusCode: statusCode
-        })
-    }
-
-    function defaultRedirectHandler(res, target, statusCode) {
-        var html =  '<html><body><h1>Moved'
-        if (statusCode === 302) {
-            html += ' Permanently'
-        }
-        html += '</h1><a href="' + target + '">' + target + '</a>'
-
-        sendHtml(res, html)
-    }
-
-    function mediaTypes(req, res, object)  {
-        var types = Object.keys(object),
-            mediaType = new Negotiator(req).preferredMediaType(types)
-
-        return object[mediaType] || object.default || 
-            createNotSupportedHandler(req, res)
-    }
-
-    function createNotSupportedHandler(req, res) {
-        return notSupportedHandler
-
-        function notSupportedHandler() {
-            errorPage(req, res, 
-                [new Error("mediaType not supported"), 415])
-        }
-    }
-
-    function methods(routes, handleHttpForms) {
-        if (handleHttpForms)  {
-            return createHttpFormsRequestHandler(routes)
-        }
-        return requestHandler
-
-        function requestHandler(req, res) {
-            var method = req.method,
-                f = routes[method]
-
-            if (f) {
-                return f.apply(this, arguments)
-            }
-            errorPage(req, res, 405)
-        }
-    }
-
-    function createHttpFormsRequestHandler(routes) {
-        return httpFormsRequestHandler
-
-        function httpFormsRequestHandler(req, res) {
-            if (req.method !== "POST") {
-                return requestHandler.apply(this, arguments)
-            }
-
-            var args = arguments,
-                self = this
-
-            contentTypes(req, {
-                "application/json": jsonBody,
-                "default": formBody
-            })(req, res, extractMethod)
-
-            function extractMethod(body) {
-                var method = body._method,
-                    f = routes[method]
-
-                if (f) {
-                    return f.apply(self, args)
-                }
-                errorPage(req, res, 405)
-            }
-        }
-    }
-
     function configure(object)  {
         extend(config, object)
     }
 
     function template(req, res, name, data) {
         Templar(req, res, config.templar)(name, data || {})
-    }
-
-    function formBody(req, res, callback)  {
-        if (!req.headers['content-type'].match(isForm)) {
-            // XXX Add support for formidable uploading, as well
-            return errorPage(req, res, 415)
-        } 
-        body(req, parseBody)
-
-        function parseBody(body) {
-            callback(querystring.parse(body))
-        }
-    }
-
-    function jsonBody(req, res, callback) {
-        if (!req.headers["content-type"].match(isJSON)) {
-            return errorPage(req, res, 415)
-        }
-        body(req, extractJSON)
-
-        function extractJSON(body) {
-            try {
-                var json = JSON.parse(body)
-            } catch (error) {
-                return errorPage(req, res, [400, error])
-            }
-            callback(json)
-        }
     }
 }
