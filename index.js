f05d7c9619c85c89490d8f0f009b39bd86ddf121
Raynos <raynos2@gmail.com>
2012-06-04 12:41:50 +0100

    refactor

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+// Third party
 var ErrorPage = require("error-page"),
-    StringDecoder = require("string_decoder").StringDecoder,
     Negotiator = require("negotiator"),
     Templar = require("templar"),
+    extend = require("xtend"),
+    // node
     querystring = require("querystring"),
-    extend = require("xtend")
-
-var static = require("./lib/static")
-
-var isJSON = /\/(x-)?json$/,
+    // internal
+    static = require("./lib/static"),
+    encoding = require("./lib/encoding"),
+    contentTypes = require("./lib/contentTypes"),
+    body = require("./lib/body"),
+    send = require("./lib/send"),
+    sendJson = send.sendJson,
+    sendHtml = send.sendHtml,
+    // variables
+    isJSON = /\/(x-)?json$/,
     isForm = /application\/x\-www\-form\-urlencoded/
     
 module.exports = Routil()
@@ -55,29 +62,38 @@ function Routil(options) {
         res.setHeader('location', target)
 
         mediaTypes(req, res, {
-            "application/json": function () {
-                sendJson(res, {
-                    redirect: target,
-                    statusCode: statusCode
-                })
-            },
-            "default": function  () {
-                var html =  '<html><body><h1>Moved'
-                if (statusCode === 302) {
-                    html += ' Permanently'
-                }
-                html += '</h1><a href="' + target + '">' + target + '</a>'
+            "application/json": jsonRedirectHandler,
+            "default": defaultRedirectHandler
+        })(res, target, statusCode)
+    }
 
-                sendHtml(res, html)
-            }
-        })()
+    function jsonRedirectHandler() {
+        sendJson(res, {
+            redirect: target,
+            statusCode: statusCode
+        })
+    }
+
+    function defaultRedirectHandler(res, target, statusCode) {
+        var html =  '<html><body><h1>Moved'
+        if (statusCode === 302) {
+            html += ' Permanently'
+        }
+        html += '</h1><a href="' + target + '">' + target + '</a>'
+
+        sendHtml(res, html)
     }
 
     function mediaTypes(req, res, object)  {
         var types = Object.keys(object),
             mediaType = new Negotiator(req).preferredMediaType(types)
 
-        return object[mediaType] || object.default || notSupportedHandler
+        return object[mediaType] || object.default || 
+            createNotSupportedHandler(req, res)
+    }
+
+    function createNotSupportedHandler(req, res) {
+        return notSupportedHandler
 
         function notSupportedHandler() {
             errorPage(req, res, 
@@ -85,88 +101,11 @@ function Routil(options) {
         }
     }
 
-    function contentTypes(req, object) {
-        var types = Object.keys(object),
-            contentType = req.headers["content-type"],
-            typeMatch
-
-        for (var i = 0; i < types.length; i++) {
-            var type = types[i]
-
-            if (contentType.indexOf(type) !== -1) {
-                typeMatch = type
-                break;
-            }
-        }
-
-        return object[typeMatch] || object.default
-    }
-
-    function encoding(req, object) {
-        var encodes = Object.keys(object),
-            encode = new Negotiator(req).preferredEncoding(encodes)
-
-        return object[encode] || object.default
-    }
-
-    function sendJson(res, object, statusCode) {
-        send(res, JSON.stringify(object), statusCode, {
-            "content-type": "application/json"
-        })
-    }
-
-    function sendHtml(res, data, statusCode) {
-        send(res, data, statusCode, {
-            "content-type": "text/html"
-        })
-    }
-
-    function send(res, data, statusCode, headers) {
-        if (!Buffer.isBuffer(data)) {
-            data = new Buffer(data)
-        }
-        
-        res.writeHead(statusCode || res.statusCode || 200, 
-            extend((headers || {}), {
-                "content-length": data.length
-            }))
-
-        res.end(data)
-    }
-
     function methods(routes, handleHttpForms) {
         if (handleHttpForms)  {
-            return httpFormsRequestHandler
+            return createHttpFormsRequestHandler(routes)
         }
         return requestHandler
-
-        function httpFormsRequestHandler(req, res) {
-            if (req.method !== "POST") {
-                return requestHandler.apply(this, arguments)
-            }
-
-            var args = arguments,
-                self = this
-
-            contentTypes(req, {
-                "application/json": function () {
-                    jsonBody(req, res, extractMethod)
-                },
-                "default": function () {
-                    formBody(req, res, extractMethod)
-                }
-            })()
-
-            function extractMethod(body) {
-                var method = body._method,
-                    f = routes[method]
-
-                if (f) {
-                    return f.apply(self, args)
-                }
-                errorPage(req, res, 405)
-            }
-        }
 
         function requestHandler(req, res) {
             var method = req.method,
@@ -179,40 +118,46 @@ function Routil(options) {
         }
     }
 
+    function createHttpFormsRequestHandler(routes) {
+        return httpFormsRequestHandler
+
+        function httpFormsRequestHandler(req, res) {
+            if (req.method !== "POST") {
+                return requestHandler.apply(this, arguments)
+            }
+
+            var args = arguments,
+                self = this
+
+            contentTypes(req, {
+                "application/json": jsonBody,
+                "default": formBody
+            })(req, res, extractMethod)
+
+            function extractMethod(body) {
+                var method = body._method,
+                    f = routes[method]
+
+                if (f) {
+                    return f.apply(self, args)
+                }
+                errorPage(req, res, 405)
+            }
+        }
+    }
+
     function configure(object)  {
         extend(config, object)
     }
 
     function template(req, res, name, data) {
-        Templar(req, res, config.templar)(name, data)
-    }
-
-    function body(req, callback) {
-        if (req.__body__) {
-            callback(req.__body__)
-        }
-
-        var requestBody = "",
-            stringDecoder = new StringDecoder
-
-        req.on("data", addToBody)
-
-        req.on("end", returnBody)
-
-        function addToBody(buffer) {
-            requestBody += stringDecoder.write(buffer)
-        }
-
-        function returnBody() {
-            req.__body__ = requestBody
-            callback(requestBody)        
-        }
+        Templar(req, res, config.templar)(name, data || {})
     }
 
     function formBody(req, res, callback)  {
         if (!req.headers['content-type'].match(isForm)) {
             // XXX Add support for formidable uploading, as well
-            errorPage(req, res, 415)
+            return errorPage(req, res, 415)
         } 
         body(req, parseBody)
 
